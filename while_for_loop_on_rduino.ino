4f601b7dce36406fee2f93fa31fbc5de7088a6e6
uwasejeannine <79199207+uwasejeannine@users.noreply.github.com>
2021-04-21 06:54:41 +0300

    Add files via upload

---
+++ while_for_loop_on_rduino.ino
@@ -0,0 +1,40 @@
+int i=1;
+//int j=1;
+int k=2;
+int Green=2;
+int Blue=3;
+int Red=4;
+
+void setup(){
+SETUP();
+//  pinMode(Green,OUTPUT);
+//  pinMode(Blue,OUTPUT);
+//  pinMode(Red,OUTPUT);
+}
+
+void loop(){
+  //(loops functions)
+//  for(i=0;i<5;i++);
+//{
+//  digitalWrite(Green, HIGH);
+//  delay(1000); // Wait for 1000 millisecond(s)
+//  digitalWrite(Green, HIGH);
+//  delay(1000); // Wait for 1000 millisecond(s)}
+//}
+//  while (j<4)
+//{
+//  digitalWrite(Blue, HIGH);
+//  delay(1000); // Wait for 1000 millisecond(s)
+//  digitalWrite(Blue, HIGH);
+//  delay(1000); // Wait for 1000 millisecond(s)}
+//}
+//  for(k=2;k<5;k++);
+//{
+//  digitalWrite(Red, HIGH);
+//  delay(1000); // Wait for 1000 millisecond(s)
+//  digitalWrite(Red, HIGH);
+//  delay(1000); // Wait for 1000 millisecond(s)}
+ void GREEN();
+ void RED();
+ void BLUE();
+}
